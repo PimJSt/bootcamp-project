48fc8f7f0f5fccfdd99334a152d169463e88e564
PimJSt <53283263+PimJSt@users.noreply.github.com>
2023-03-03 00:14:31 +0700

    Add files via upload

---
+++ R/hw_chatbot.r
@@ -0,0 +1,80 @@
+# Homework 01
+# chatbot (rule-based)
+# ordering pizza
+
+# in replit use readLines to get char input
+#print("What's your name?") # nolint
+#user <- readLines("stdin", n = 1) # nolint
+#print(paste("Hi", user)) # nolint
+
+
+topping <- c("Cheese", "Pepporoni", "Meat Lover", "Hawaiian", "Supreme")
+crust <- c("Thin", "Thick")
+size <- c("Small", "Medium", "Large")
+appe <- c("Garlic Bread", "Chicken Wings")
+drinks <- c("Coke", "Sprite", "Water")
+
+order_pizza <- function() {
+    print("What would you like to order?")
+    print(paste(seq_along(topping), topping))
+    get_topping <- as.numeric(readline())
+
+    print("Which crust style would you like?")
+    print(paste(seq_along(crust), crust))
+    get_crust <- as.numeric(readline())
+
+    print("Which size?")
+    print(paste(seq_along(size), size))
+    get_size <- as.numeric(readline())
+
+    return(paste(size[get_size], crust[get_crust], topping[get_topping]))
+}
+
+order_appe <- function() {
+    print("What kind of appetizer?")
+    print(paste(seq_along(appe), appe))
+    get_index <- as.numeric(readline())
+
+    return(appe[get_index])
+}
+
+order_drink <- function() {
+    print("What kind of drinks?")
+    print(paste(seq_along(drinks), drinks))
+    get_index <- as.numeric(readline())
+
+    return(drinks[get_index])
+}
+
+
+# Main
+
+order_list <- c()
+
+print("Welcome to our pizza shop")
+name <- readline("What's your name? ")
+print(paste("Hi,", name))
+pizza <- order_pizza()
+order_list <- append(order_list, pizza)
+
+while (TRUE) {
+    print("Anything else?")
+    print("1 Pizza  2 Appetizer  3 Drinks  4 Nothing else")
+    get_else <- as.numeric(readline())
+    if (get_else == 1) {
+        get_pizza <- order_pizza()
+        order_list <- append(order_list, get_pizza)
+    } else if (get_else == 2) {
+        get_appe <- order_appe()
+        order_list <- append(order_list, get_appe)
+    } else if (get_else == 3) {
+        get_drink <- order_drink()
+        order_list <- append(order_list, get_drink)
+    } else {
+        order_df <- data.frame(table(order_list))
+        names(order_df) <- c("Menu", "Amount")
+        print(paste(name, "orders", nrow(order_df), "items"))
+        print(order_df)
+        break()
+    }
+}
---
+++ R/hw_junken.r
@@ -0,0 +1,44 @@
+# Homework 02
+# Junken
+# play unlimited times
+# summary if player quit
+
+choice <- c("r", "p", "s")
+print("r for Rock, p for Paper, s for Scissors or press q to quit")
+
+game <- 1
+win_game <- 0
+lose_game <- 0
+draw_game <- 0
+
+while (game > 0) {
+    bot <- sample(choice, size = 1)
+    get_rps <- readline("Rock Paper Scissors ? ")
+    win <- (get_rps == "r" && bot == "s") ||
+           (get_rps == "p" && bot == "r") ||
+           (get_rps == "s" && bot == "p")
+    lose <- (get_rps == "r" && bot == "p") ||
+            (get_rps == "p" && bot == "s") ||
+            (get_rps == "s" && bot == "r")
+
+    if (get_rps == "q") {
+        print(paste("You played ", game - 1, " games"))
+        print(paste("Win ", win_game))
+        print(paste("Lose ", lose_game))
+        print(paste("Draw ", draw_game))
+        break
+    } else if (win) {
+        print("Won")
+        win_game <- win_game + 1
+    } else if (lose) {
+        print("Lose")
+        lose_game <- lose_game + 1
+    } else if (get_rps == bot) {
+        print("Draw")
+        draw_game <- draw_game + 1
+    } else {
+        print("Try again")
+        game <- game - 1
+    }
+    game <- game + 1
+}
